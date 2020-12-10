1732aee458e9d416d982c43382deab88e65d7b63
Yosuke-Aramaki <gnx.vw903@gmail.com>
2020-12-10 17:11:40 +0900

    add test code certificate

--- test/Contract.test.ts
+++ test/Contract.test.ts
@@ -21,8 +21,12 @@ describe('ERC1155Certificate', () => {
   });
 
   it('check issued Certificate', async () => {
-    // let result = await contractInstance.certificates(1);
-    // console.log(result)
+    let result = await contractInstance.certificates(0);
+    expect(result.nameOfCertificate).to.equal("Certificate No.1");
+    expect(result.issuer).to.equal(issuer1.address);
+    result = await contractInstance.certificates(1);
+    expect(result.nameOfCertificate).to.equal("Certificate No.2");
+    expect(result.issuer).to.equal(issuer2.address);
   });
 
   it('Issue certificates to receiver', async () => {
@@ -44,40 +48,4 @@ describe('ERC1155Certificate', () => {
     expect(result[0]).to.equal(1);
     // expect(result[1]).to.equal(2);
   })
-
-
-  // it('Assigns initial balance', async () => {
-  //   expect(await token.balanceOf(wallet.address)).to.equal(1000);
-  // });
-
-  // it('Transfer adds amount to destination account', async () => {
-  //   await token.transfer(walletTo.address, 7);
-  //   expect(await token.balanceOf(walletTo.address)).to.equal(7);
-  // });
-
-  // it('Transfer emits event', async () => {
-  //   await expect(token.transfer(walletTo.address, 7))
-  //     .to.emit(token, 'Transfer')
-  //     .withArgs(wallet.address, walletTo.address, 7);
-  // });
-
-  // it('Can not transfer above the amount', async () => {
-  //   await expect(token.transfer(walletTo.address, 1007)).to.be.reverted;
-  // });
-
-  // it('Can not transfer from empty account', async () => {
-  //   const tokenFromOtherWallet = token.connect(walletTo);
-  //   await expect(tokenFromOtherWallet.transfer(wallet.address, 1))
-  //     .to.be.reverted;
-  // });
-
-  // it('Calls totalSupply on BasicToken contract', async () => {
-  //   await token.totalSupply();
-  //   expect('totalSupply').to.be.calledOnContract(token);
-  // });
-
-  // it('Calls balanceOf with sender address on BasicToken contract', async () => {
-  //   await token.balanceOf(wallet.address);
-  //   expect('balanceOf').to.be.calledOnContractWith(token, [wallet.address]);
-  // });
 });
